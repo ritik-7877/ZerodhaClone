2d916e1317dfbd9156616bda398a0c9c11b9e0eb
ritik-7877 <165564135+ritik-7877@users.noreply.github.com>
2026-02-19 18:44:38 +0530

    Update some miner functionalities

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -7,7 +7,17 @@ function Education() {
                     <img src='media/images/homeHero.png' alt='Hero Image' />
                     <h1> Invest in everything </h1>
                     <p> Online platform to invest in stocks, derivatives, matual funds, and more </p>
-                    <button className="p-2 m-auto fs-5 btn btn-primary" style={{width:"20%"}}>Signup now</button>
+                    <div className="row text-center mt-5 mb-5">
+                    <div className="col-12">
+                        <Link to="/signup">
+                            <button
+                                className="btn btn-primary fs-5 px-4 mx-auto d-block"
+                            >
+                                Signup Now
+                            </button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
