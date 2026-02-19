18107f2d7340e6f41dfe9ea078a8b81c612f42f0
ritik-7877 <165564135+ritik-7877@users.noreply.github.com>
2026-02-19 18:32:25 +0530

    Update authcontroller

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -2,51 +2,71 @@ const User = require("../Models/UserModel");
 const { createSecretToken } = require("../util/SecretToken");
 const bcrypt = require("bcrypt");
 
-module.exports.Signup = async (req, res, next) => {
+module.exports.Signup = async (req, res) => {
   try {
     const { email, password, username, createdAt } = req.body;
+
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.json({ message: "User already exists" });
+      return res.status(400).json({ message: "User already exists" });
     }
+
     const user = await User.create({ email, password, username, createdAt });
 
     const token = createSecretToken(user._id);
+
     res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
     });
-    res
-      .status(201)
-      .json({ message: "User signed in successfully", success: true, user });
-    next();
+
+    res.status(201).json({
+      message: "User signed up successfully",
+      success: true,
+      user,
+    });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ success: false });
   }
 };
 
-module.exports.Login = async (req, res, next) => {
+
+module.exports.Login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if(!email || !password ){
-      return res.json({message:'All fields are required'})
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
     const user = await User.findOne({ email });
-    if(!user){
-      return res.json({message:'Incorrect password or email' }) 
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    const auth = await bcrypt.compare(password,user.password)
+
+    const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
-      return res.json({message:'Incorrect password or email' }) 
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-     const token = createSecretToken(user._id);
-     res.cookie("token", token, {
-       withCredentials: true,
-       httpOnly: false,
-     });
-     res.status(201).json({ message: "User logged in successfully", success: true });
-     next()
+
+    const token = createSecretToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      success: true,
+    });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ success: false });
   }
-}
+};
